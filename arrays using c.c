0d2823b9e39c7dfb44fbcd7e9e2a2d9138e0f232
nitin-0-7 <nitinkrc2432@gmail.com>
2025-05-30 11:33:10 +0530

    Add files via upload

---
+++ arrays using c.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int marks[4];
+  printf("Enter the value of of marks for student 1 :");
+  scanf("%d",&marks[0]);
+  printf("Enter the value of of marks for student 2 :");
+  scanf("%d",&marks[1]);
+  printf("Enter the value of of marks for student 3 :");
+  scanf("%d",&marks[2]);
+  printf("Enter the value of of marks for student 4 :");
+  scanf("%d",&marks[3]);
+      printf("you have enterd %d %d %d and %d",marks[0],marks[1],marks[2],marks[3]);                                                                                                                                                                                                                                                  
+    return 0;
+}
